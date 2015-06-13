9240a760168a693405afd8dd331c05c537d1ab4c
semimono <culleneason@gmail.com>
2015-06-13 10:37:31 -0600

    Added functions for loading external libraries.

--- Source/Boxel/Private/Boxel.cpp
+++ Source/Boxel/Private/Boxel.cpp
@@ -3,6 +3,32 @@
 #include "BoxelPrivatePCH.h"
 #include "BoxelEditorMode.h"
 #include "BoxelEditorStyle.h"
+#include <windows.h>
+
+void* loadLibrary(const FString& fileName) {
+	FString filePath = FPaths::Combine(*FPaths::GamePluginsDir(), TEXT("ExternalBin/Win64/")) +fileName; // Concatenate the plugins folder and the DLL file.
+
+	void *libraryHandle = NULL;
+	if (FPaths::FileExists(filePath))
+	{
+		libraryHandle = FGenericPlatformProcess::GetDllHandle(*filePath); // Retrieve the DLL.
+		if (libraryHandle == NULL) {
+			UE_LOG(LogTemp, Error, TEXT("Failed to load library: %s"), *filePath);
+		}
+	} else {
+		UE_LOG(LogTemp, Error, TEXT("Failed to load library; non-existant file: %s"), *filePath);
+	}
+	return libraryHandle;
+}
+
+void closeLibrary(void* libraryHandle) {
+	FGenericPlatformProcess::FreeDllHandle(libraryHandle);
+}
+
+void* lookupFunction(void* libraryHandle, const FString& funcName) {
+	return FGenericPlatformProcess::GetDllExport(libraryHandle, *funcName);
+}
+
 
 
 class FBoxelModule : public IBoxelModule
